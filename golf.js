e3cc9385369a60d68c06d5a53d4d45eb4c06ef6b
AitorAstorga <44289776+AitorAstorga@users.noreply.github.com>
2018-11-30 12:32:40 +0100

    Add files via upload

---
+++ golf.js
@@ -0,0 +1,11 @@
+function inicio (){
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = 10;
+var y = canvas.height/2;
+var dx
+var dy
+
+
+setInterval(draw, 20);
+}
